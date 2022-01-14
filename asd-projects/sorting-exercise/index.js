81e1c270d46fc0c0a285dd82cdf76d991a0203c5
jereholmes <holmesjeremiah756@gmail.com>
2022-01-14 23:46:20 +0000

    saving sorting exercise

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -47,8 +47,8 @@ async function quickSort(array, left, right){
 // TODOs 4 & 5: Implement partition
 async function partition(array, left, right){
   var pivot = array[Math.floor((right + left)/2)].value;
-  while(left < right){
-      
+  while(array[left].value < right){
+      left++
   }
   return left + 1
 }
